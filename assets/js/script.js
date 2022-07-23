f832b2105ea013e059877393e609ad984cb3b44b
Lucas Reis <lucasreis_cod@hotmail.com>
2022-07-23 20:52:41 -0300

    Add estilo do header, do banner e o menu com JS

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// MENU
+ let btnMenu = document.getElementById('btnmenu');
+ let menu = document.getElementById('menu')
+
+ btnMenu.addEventListener('click', ( )=>{  
+    'use strict';
+    menu.classList.toggle('mostrar');
+ })
